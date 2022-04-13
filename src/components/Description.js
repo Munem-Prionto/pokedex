fb33d2811016d53da71d956b14fe3e896f59bfb1
Munem-Prionto <munem.prionto@gmail.com>
2022-04-14 02:47:39 +0600

    desc eng

--- src/components/Description.js
+++ src/components/Description.js
@@ -5,5 +5,5 @@ import useFetch from '../hooks/useFetch'
 export default function Description({id}) {
     const {data : species} = useFetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`)
 
-  return species && <p>{species.flavor_text_entries[0].flavor_text}</p>
+  return species && <p>{species.flavor_text_entries[1].flavor_text}</p>
 }
